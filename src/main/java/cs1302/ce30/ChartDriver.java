7e4c29670f1cf20a18b136e2a38f8fcd384119c2
bjb211 <bjb211@uga.edu>
2019-04-13 17:13:18 -0400

    Initial Commit. Contains starter code

---
+++ src/main/java/cs1302/ce30/ChartDriver.java
@@ -0,0 +1,17 @@
+package cs1302.ce30;
+
+import javafx.application.Application;
+
+public class ChartDriver {
+
+    public static void main(String[] args) {
+        try {
+            Application.launch(ComplexityClasses.class, args);
+        } catch (UnsupportedOperationException e) {
+            System.err.println(e);
+            System.err.println("Likely due to X11 timeout. Logout and log back in...");
+            System.exit(1);
+        } // try
+    } // main
+
+} // ChartDriver
---
+++ src/main/java/cs1302/ce30/ComplexityClasses.java
@@ -0,0 +1,94 @@
+package cs1302.ce30;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.chart.LineChart;
+import javafx.scene.Scene;
+import java.util.function.Function;
+
+/**
+ * A JavaFX application containing a {@code LineChart} with series for each of the following
+ * complexity classes: <br>
+ *  - Constant Time <br>
+ *  - Logarithmic Time <br>
+ *  - Linear Time <br> 
+ *  - Linearithmic Time (nlogn) <br>
+ *  - Quadratic Time <br>
+ *  - Cubic Time <br>
+ *  - Exponential Time <br>
+ *  - Factorial Time <br>
+ */
+public class ComplexityClasses extends Application {
+
+    /** Problem size (size of the array) */
+    private static final int N = 100;
+    
+    /** Bound for the Y-axis in the LineChart */
+    private static final int Y_BOUND = 10000;
+
+    /**
+     * The entry point for the JavaFX application
+     */
+    public void start(Stage stage) {
+        Integer[] x = new Integer[N];
+
+        // Initialize the values for the x-axis (Problem Size)
+        for(int i = 0; i < x.length; i++) {
+            x[i] = i;
+        } // for
+
+        // Constant Time
+        Double[] y = genData(N, val -> 10.0);
+
+        // Create initial line chart and add the constant time data series
+        LineChart<Number, Number> lc = ChartUtility.createChart(x, y, "Size",
+                                                                "Operations", Y_BOUND, "nlogn");
+        lc.setTitle("Complexity Classes");
+
+        // Add the linear time data series to the line chart 
+        ChartUtility.addSeries(lc, x, genData(N, val->val.doubleValue()), "Linear");
+       
+        // linearithmic
+                               
+        // exponential
+                               
+        // logarithmic
+
+        // quadratic
+
+        // cubic
+
+
+        Scene scene = new Scene(lc);
+
+        // Add the css styling to remove line symbols
+        scene.getStylesheets().add("chartStyle.css");
+                               
+        stage.setTitle("Complexity Classes");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    } // start
+
+    /**
+     * A method to create and populate an array of {@code Double} objects of a particular size.
+     * Each value in the array is its index value mapped to a double value by the specified
+     * {@code Function} object.  
+     * For example, a call to 
+     * <pre>
+     *    genData(2, val -&#62; val.doubleValue() + 10.0);
+     * </pre>
+     * Would return [10.0, 11.0]. Each value in the array of size 2 is its index value + 10.0 as
+     * defined by {@code f}.
+     *
+     * @return the double array containing the mapped index values
+     * @param size the size of the array to create
+     * @param f the function to map index values to doubles
+     */
+    public Double[] genData(int size, Function<Integer, Double> f) {
+
+        // TODO: Implement Me!
+
+    } // genData
+    
+} // Application
