9d8c05ad08c1e2bef0315d39d4897f3898c86a9d
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-04-14 14:40:08 -0400

    updated starter code

--- src/main/java/cs1302/ce29/ComplexityClasses.java
+++ src/main/java/cs1302/ce29/ComplexityClasses.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
  * complexity classes: <br>
  *  - Constant Time <br>
  *  - Logarithmic Time <br>
- *  - Linear Time <br> 
+ *  - Linear Time <br>
  *  - Linearithmic Time (nlogn) <br>
  *  - Quadratic Time <br>
  *  - Cubic Time <br>
@@ -22,49 +22,47 @@ public class ComplexityClasses extends Application {
 
     /** Problem size (size of the array) */
     private static final int N = 100;
-    
+
     /** Bound for the Y-axis in the LineChart */
     private static final int Y_BOUND = 10000;
+
+    /** Line chart for plots. */
+    private LineChart<Number, Number> lc;
 
     /**
      * The entry point for the JavaFX application
      */
     public void start(Stage stage) {
-        Integer[] x = new Integer[N];
 
         // Initialize the values for the x-axis (Problem Size)
+	Integer[] x  = new Integer[N];
         for(int i = 0; i < x.length; i++) {
             x[i] = i;
         } // for
 
-        // Constant Time
-        Double[] y = genData(N, val -> 10.0);
+        // Constant Time Example
+	Double[] y = genData(N, val -> 10.0);
 
         // Create initial line chart and add the constant time data series
-        LineChart<Number, Number> lc = ChartUtility.createChart(x, y, "Size",
-                                                                "Operations", Y_BOUND, "nlogn");
+        lc = ChartUtility.createChart(x, y, "Size", "Operations", Y_BOUND, "Constant");
         lc.setTitle("Complexity Classes");
 
-        // Add the linear time data series to the line chart 
+        // Add the linear time data series to the line chart
         ChartUtility.addSeries(lc, x, genData(N, val->val.doubleValue()), "Linear");
-       
+
         // linearithmic
-                               
+
         // exponential
-                               
+
         // logarithmic
 
         // quadratic
 
         // cubic
 
-
         Scene scene = new Scene(lc);
-
-        // Add the css styling to remove line symbols
-        scene.getStylesheets().add("chartStyle.css");
-                               
-        stage.setTitle("Complexity Classes");
+        scene.getStylesheets().add("chartStyle.css"); // use CSS to remove line symbols
+        stage.setTitle("Complexity Classes");         // set app title
         stage.setScene(scene);
         stage.sizeToScene();
         stage.show();
@@ -73,8 +71,8 @@ public class ComplexityClasses extends Application {
     /**
      * A method to create and populate an array of {@code Double} objects of a particular size.
      * Each value in the array is its index value mapped to a double value by the specified
-     * {@code Function} object.  
-     * For example, a call to 
+     * {@code Function} object.
+     * For example, a call to
      * <pre>
      *    genData(2, val -&#62; val.doubleValue() + 10.0);
      * </pre>
@@ -86,9 +84,8 @@ public class ComplexityClasses extends Application {
      * @param f the function to map index values to doubles
      */
     public Double[] genData(int size, Function<Integer, Double> f) {
-
         // TODO: Implement Me!
-
+	throw new UnsupportedOperationException("not yet implemented");
     } // genData
-    
-} // Application
+
+} // ComplexityClasses
