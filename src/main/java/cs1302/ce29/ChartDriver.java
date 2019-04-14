69685377f05b873881fb038c1da6ac6abba00b4d
bjb211 <bjb211@uga.edu>
2019-04-14 13:45:15 -0400

    updated Javadoc

--- src/main/java/cs1302/ce29/ChartDriver.java
+++ src/main/java/cs1302/ce29/ChartDriver.java
@@ -2,6 +2,10 @@ package cs1302.ce29;
 
 import javafx.application.Application;
 
+/**
+ * The main entry point for our application. This class launches
+ * the {@code ComplexityClasses} class and catches X11 timeout errors.
+ */
 public class ChartDriver {
 
     public static void main(String[] args) {
